d14b45471c8ee92ab222625062f60c33c57eebe8
Ilya Kochurov <leon.coxsw@gmail.com>
2025-08-29 14:08:39 +0400

    JavaSpringMock project init

---
+++ src/main/java/com/example/JavaSpringMock/JavaSpringMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringMock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringMockApplication.class, args);
+    }
+
+}
